4554f493582ed4e2bff1a26973e3cd4fee50d60c
Timofei <tictacnumt@gmail.com>
2024-02-06 10:42:00 +0300

    set command almost ready for integer numbers

--- ProjectFiles/CustomLibs/src/Command_system.c
+++ ProjectFiles/CustomLibs/src/Command_system.c
@@ -10,4 +10,26 @@ void execute_command(char *command) {
     int freq = atoi((char *)(command + strlen("set freq ")));
       set_DAC_table(freq);
   }
+  else if (strstr(command, "set ") == command) {
+    int i = 4;                                                                    // 1ый символ числа num
+    int buffer_num = 0;                                                           // буферная переменная
+    int j = 0;                                                                    // индекс таблицы DAC_table 
+    while ((command[i] != 0) && (command[i] != '\n') && (command[i] != '\r')) {   // пока строка не закончена
+      if ((command[i] >= '0') && (command[i] <= '9')) {                           // проверка того, что это цифра
+        buffer_num *= 10;                                                         // новый разряд числа
+        buffer_num += (command[i] - '0');                                         // прибавить разряд
+        i += 1;                                                                   // инкрементировать 
+      }
+      else if (command[i] == ',') {
+        i += 2;                                                                   // пропустить сиволы ',' и ' '
+        DAC_table[j] = buffer_num;                                                // занести новое значение
+        j++;                                                                      // инкрементировать индекс таблицы DAC_table
+      }
+      else {
+        break;                                                                    // допущенна ошибка при вводе
+      }
+    }
+    
+  }
+  
 }
