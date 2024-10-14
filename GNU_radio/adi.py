62179a549ba15c67588ba71002984a409515ec79
Timofei <tictacnumt@gmail.com>
2024-10-14 10:48:35 +0300

    foo

--- GNU_radio/adi.py
+++ GNU_radio/adi.py
@@ -18,8 +18,6 @@ MAX_DAC_NUM = 500   # Максимальное количество чисел, 
 MAX_DAC_VAL = 10.0  # Максимальное значение на выходе ЦАП
 CHUNK_SIZE = 10     # Максимальное количество чисел в одной команде
 USB_PACKET_SIZE = 32 # байт
-# USB_PACKET_SIZE = 256 # байт              ДЛЯ dac mode
-
 
 class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
     """
@@ -31,9 +29,7 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
     
     def __init__(self, portNumber=7, mode = 3): 
         """arguments to this function show up as parameters in GRC"""
-        self.portNumber = portNumber  # Сохраняем номер порта
-        # portName = 'COM' + str(portNumber)                  # Concatenate string and number
-        
+        self.portNumber = portNumber  # Сохраняем номер порта       
         gr.sync_block.__init__(
             self,
             name = 'Analog Digital Interface',   
@@ -45,26 +41,26 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
         self.message_port_register_in(pmt.intern('set_const_signal'))
         self.set_msg_handler(pmt.intern('set_const_signal'), self.handle_msg)
         # self.message_port_register_out(pmt.intern('msg_out'))
-
-         # Инициализация переменных для COM порта
+        self.set_min_output_buffer(2**13)        # 512 - минимальный размер
+        self.set_max_output_buffer(2**13)  # Установка максимального размера буфера
+        self.set_output_multiple(2**13)     # Установка шага данных
+        # Инициализация переменных для COM порта
         self.port = None
         self.port_name = None
         self.baudrate = 2000000  # Здесь захардкожен baudrate
         self.mode = mode
         self.remaining_data = bytearray(USB_PACKET_SIZE + 1)  # Буфер для оставшихся данных     0ой байт - флаг (0 - в массиве нет полезных данных, 1 - надо читать)
         
+
     def open_port(self):
         """Открывает COM порт, если он существует и не занят."""
         baudrate = 2000000
         portName = 'COM' + str(self.portNumber)
-
         # Получаем список доступных COM портов
         available_ports = [port.device for port in serial.tools.list_ports.comports()]
-
         if portName not in available_ports:
             print(f"Ошибка: Порт {portName} не найден.")
             return
-        
         try:
             # Пытаемся открыть порт
             self.port = serial.Serial(portName, baudrate, timeout=3.0)
@@ -99,25 +95,16 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
         # Преобразование сообщения в список чисел с плавающей запятой
         numbers = pmt.to_python(msg)        # Преобразование в строку
         numbers = [float(x) for x in numbers.split()]
-        print("AAAAAAAAAAAAAAAAAAAAAA")
-        # print(numbers[30])
-
-        # if not isinstance(numbers, list):
-        #     print("Ошибка: Ожидался список чисел.")
-        #     return
-
+        print("Обработка сообщения")
         if len(numbers) > MAX_DAC_NUM:
             print(f"Ошибка: Слишком много чисел. Максимум {MAX_DAC_NUM}.")
             return
-
         if any(val > MAX_DAC_VAL for val in numbers):
             print(f"Ошибка: Значения не могут превышать {MAX_DAC_VAL}.")
             return
-
         # Открытие порта, если еще не открыт
         if self.port is None:
             self.open_port()
-
         if self.port is not None and self.port.is_open:
             # Разделение данных на куски по 10 чисел и отправка
             for i in range(0, len(numbers), CHUNK_SIZE):
@@ -125,14 +112,12 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
                 command = "set " + " ".join(map(str, chunk))
                 if i + CHUNK_SIZE >= len(numbers):
                     command += " !"
-                
                 try:
                     self.port.write(command.encode('ascii'))
                     print(f"Отправлено: {command}")
                     time.sleep(0.01)
                 except serial.SerialException as e:
                     print(f"Ошибка при отправке данных: {e}")
-
 
             
     def work(self, input_items, output_items):
@@ -147,11 +132,9 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
             n = 0
             chunk_size = USB_PACKET_SIZE
             output_len = len(output_items[0])
-            # print (output_len)
             data = bytearray(chunk_size)  # Создание массива байтов для чтения данных
             n_increment = (USB_PACKET_SIZE if self.mode < 3 else (USB_PACKET_SIZE // 2))
 
-                
             while n < (output_len - n_increment):
                 chunk_size = USB_PACKET_SIZE
             
@@ -161,13 +144,6 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
                     chunk_size = len(self.remaining_data) - 1
                     data = bytearray(chunk_size)
                     data = self.remaining_data[1:]       # Сохранить данные 
-                    # print (data)
-                    # data[-1:] = b'\x00'
-                    # print(len(self.remaining_data))
-                    # print(data)
-                    # print("YES")
-                    # while True:
-                    #     pass
                     self.remaining_data = bytearray(USB_PACKET_SIZE + 1)  # Очищаем буфер
                 else:
                     data = bytearray(chunk_size)  # Создание массива байтов для чтения данных
@@ -180,10 +156,7 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
                     # TEST 8 bit
                     output_items[0][n:n + chunk_size // 2] = data[0::2]
                     output_items[1][n:n + chunk_size // 2] = data[1::2]
-                    # output_items[1][n] = b'\x30'
                     n += chunk_size // 2
-
-                    # TESST
 
             # Обработка оставшегося места в output_items
             if n < output_len:
@@ -195,25 +168,17 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
                     self.remaining_data = data[bytes_to_copy:]  # Сохраняем оставшиеся данные
                 else:
                     i = 0
-                    # print(n)
                     while n < output_len and i < chunk_size:
                         output_items[0][n] = data[i] 
                         output_items[1][n] = data[i + 1]
                         n += 1
                         i += 2
-                    # output_items[1][n-1] = 300
                     self.remaining_data = bytearray(USB_PACKET_SIZE - i + 1)
                     self.remaining_data[1:] = data[i:]  # Сохраняем оставшиеся данные
                     self.remaining_data[0] = (0 if (USB_PACKET_SIZE - i) == 0 else 1)  # Флаг запроса чтения
-                    # print(n)
-                    # print(self.remaining_data)
 
             return len(output_items[0])  # Возвращаем количество обработанных элементов
 
-
-
-
-                # Случай, когда mode >= 4 (dac_mode) - упор на ЦАП, АЦП отключен
         # Случай, когда mode >= 4 (dac_mode) - упор на ЦАП, АЦП отключен
         else:
             data = bytearray(USB_PACKET_SIZE )  # Создание массива байтов для чтения данных
@@ -230,40 +195,34 @@ class ADIBlock(gr.sync_block):  # other base classes are basic_block, decim_bloc
 
             while n < input_len:
                 i = remaining_len  # Сбрасываем индекс для data
-
                 while i < (USB_PACKET_SIZE ) and n < input_len:  # Проверяем и n
-                    value1 = int((input_items[0][n] + 1) * 4000 / 2) & 0xFFFF  # Приведение к целому числу и ограничение до 16 бит
+                    value1 = int((input_items[0][n]) * 4000) & 0xFFF  # Приведение к целому числу и ограничение до 16 бит
                     data[i] = value1 & 0xFF  # Младший байт
                     data[i + 1] = (value1 >> 8) & 0xFF  # Старший байт
                     i += 2
-                    value2 = int((input_items[1][n] + 1) * 4000 / 2) & 0xFFFF  # Приведение к целому числу и ограничение до 16 бит
+                    value2 = int((input_items[1][n]) * 4000) & 0xFFF  # Приведение к целому числу и ограничение до 16 бит
                     data[i] = value2 & 0xFF  # Младший байт
                     data[i + 1] = (value2 >> 8) & 0xFF  # Старший байт
                     i += 2
                     n += 1
-
                 # Отправляем собранные данные в порт
                 self.port.write(data)  # Отправляем только заполненную часть
                 remaining_len = 0
                 data = bytearray(USB_PACKET_SIZE )
-
             # Обработка оставшихся данных
             if n < input_len:
                 remaining_data = bytearray()
-
                 while n < input_len and i < USB_PACKET_SIZE :
-                    value1 = int((input_items[0][n] + 1) * 4000 / 2) & 0xFFFF
+                    value1 = int((input_items[0][n]) * 4000) & 0xFFF
                     remaining_data.append(value1 & 0xFF)
                     remaining_data.append((value1 >> 8) & 0xFF)
                     i += 2
-                    value2 = int((input_items[1][n] + 1) * 4000 / 2) & 0xFFFF
+                    value2 = int((input_items[1][n]) * 4000) & 0xFFF
                     remaining_data.append(value2 & 0xFF)
                     remaining_data.append((value2 >> 8) & 0xFF)
                     i += 2
                     n += 1
-
                 # Сохраняем остаточные данные для следующего вызова
                 self.remaining_data = remaining_data
 
             return len(input_items[0])
-
--- GNU_radio/filter_IQ.py
+++ GNU_radio/filter_IQ.py
@@ -10,35 +10,27 @@ import numpy as np
 from gnuradio import gr
 import enum
 
-
-# class Scheduler(enum.Enum):
-#     first_channel = 0
-#     second_channel = 1
-#     deinit = 2
-
-
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
     """Embedded Python Block example - a simple multiply const"""
 
-    def __init__(self, example_param=1.0):  # only default arguments here
+    def __init__(self, modulation_type = 0):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
-            name='Filter IQ QPSK',   # will show up in GRC
+            name='Filter IQ',   # will show up in GRC
             in_sig=[np.float32, np.float32],
             out_sig=[np.float32, np.float32]
         )
-        # if an attribute with the same name as a parameter is found,
-        # a callback is registered (properties work, too).
-        self.example_param = example_param
+        self.modulation_type = modulation_type
 
     def work(self, input_items, output_items):
-        WAIT = 10
+        cnt_start_cond = 0
+        WAIT = 10 * 8 * 4
         packet_size_bytes = 10  # Размер пакета в байтах
         packet_size_bits = packet_size_bytes * 8  # 10 байт = 80 бит
         bit_size = 4  # Размер бита (для расчета шага при чтении)
+        packet_size_mes = packet_size_bits * bit_size  # 10 байт = 80 бит
         # "Планировщик" распределяющий по каналам (кто раньше 0->1, тот первый канал) 
-        # scheduler = Scheduler.first_channel
         scheduler = 0
 
         # 1 - Заполнить выходной массив нулями
@@ -54,8 +46,6 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
 
         output_len = len(output_items[0])
         input_len = len(input_items[0])
-
-        # print(input_len, output_len)
         
 
         chan_0_start_condition = False
@@ -69,17 +59,24 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         read_started = False  # Флаг, началось ли чтение
         bits_read = 0  # Счетчик прочитанных битов
 
+        # Для нормализации будем делить заносимые значения на амплитуду
+        amplitude = max(input_items[0])
+        # Амплитуда не должна быть меньше 0.8
+        if amplitude < 0.8:
+            amplitude = 1.0
+        threshold = amplitude * 0.4
+        
+        # print(input_len)
         while i < (input_len - 7):
             if not start_condition_found:
-                # 1 - Либо одновременно, либо с шагом в 1 оба канала должны перейти в 1 
-                # 1 - Проверяем условие: оба канала последовательно перешли на 1
-                if input_items[0][i] > 0.4:
+            # 1 - Найти start_condition
+                if input_items[0][i] > threshold:
                     if chan_0_start_condition == False:
                         chan_0_start_index = (i + 2 + 4)
                         chan_num_0 = scheduler            # каким каналом назначен первый
                         scheduler += 1
                     chan_0_start_condition = True
-                if input_items[1][i] > 0.4:
+                if input_items[1][i] > threshold:
                     if chan_1_start_condition == False:
                         chan_1_start_index = (i + 2 + 4)
                         chan_num_1 = scheduler            # каким каналом назначен второй
@@ -87,37 +84,29 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
                     chan_1_start_condition = True
 
                 if (chan_0_start_condition and chan_1_start_condition):
-                    if (abs(chan_0_start_index - chan_1_start_index) < 4):
+                    if (abs(chan_0_start_index - chan_1_start_index) < 8):
                         start_condition_found = True
+                        cnt_start_cond += 1
                         read_started = True
                         bits_read = 0  # Сбрасываем счетчик прочитанных битов
                     else:
                         chan_0_start_condition = False
                         chan_1_start_condition = False
-                        # scheduler = Scheduler.first_channel
                         scheduler = 0
-                    # # Посчитаем точно сколько измерений приходится на 1 бит
-                    # if prev_start != 0:
-                    #     count_mes_per_pack = i - prev_start
-                    #     exact_num_of_mes_per_bit = count_mes_per_pack / packet_size_bits
-                    #     print(exact_num_of_mes_per_bit)
-                    # prev_start = i
-
-                # if input_items[0][i] > 0.5 and input_items[1][i] > 0.5:
-                #     start_condition_found = True
-                #     read_started = True
-                #     bits_read = 0  # Сбрасываем счетчик прочитанных битов
             else:
                 if read_started and (bits_read < packet_size_bits):
                     # 2 - Читаем биты с текущего индекса с шагом exact_num_of_mes_per_bit
                     if (i - chan_0_start_index) >= 0 and (i - chan_0_start_index) % exact_num_of_mes_per_bit < 1.0:
-                        output_items[chan_num_0][j // 2] = (1 if input_items[0][i] > 0.5 else 0)
+                        # output_items[chan_num_0][j // 2] = (1 if input_items[0][i] > 0.5 else 0)
+                        output_items[chan_num_0][j // 2] = (input_items[0][i] / amplitude)
                         j += 1
                         bits_read += 1
                     if (i - chan_1_start_index) >= 0 and (i - chan_1_start_index) % exact_num_of_mes_per_bit < 1.0:
-                        output_items[chan_num_1][j // 2] = (1 if input_items[1][i] > 0.5 else 0)
+                        # output_items[chan_num_1][j // 2] = (1 if input_items[1][i] > 0.5 else 0)
+                        output_items[chan_num_1][j // 2] = (input_items[1][i] / amplitude)
                         j += 1
                         bits_read += 1
+                        # print(bits_read, i)
                 else:
                     # Чтение завершено, сбрасываем флаг и ищем новое start_condition
                     start_condition_found = False
@@ -126,11 +115,16 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
                     chan_1_start_condition = False
                     bits_read = 0
                     scheduler = 0
-                    j += (packet_size_bits  + 1) * 4 - packet_size_bits  # (packet_size_bits * 2)    - нули и полезная нагрузка
-                                                                            # (+ 1) - 0x3               - начало фрейма
-                                                                            # (- packet_size_bits)      - мы уже передали 1 пакет битов, надо их учесть
-                    i += (WAIT // 2)
+                    j += int(packet_size_bits * 4 / (self.modulation_type + 1))
 
+                    stop_trig = 0
+                    while (i < input_len and (stop_trig < 4)):
+                        if ((input_items[0][i] < threshold) and (input_items[0][i] < threshold)):
+                            stop_trig += 1
+                        else:
+                            stop_trig = 0
+                        i += 1
+                    test_mark = i
             i += 1  # Шаг в любом случае
-    
+            
         return output_len
